b3fe423c044e94f8d2030dbde661fd26257e7e35
German Arcos <arcosgerman28@gmail.com>
2018-02-21 21:18:16 +0100

    21/02

---
+++ Tema9/cosa/src/cosa/main.java
@@ -0,0 +1,14 @@
+package cosa;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+		for (i = 0; i<5;i++){
+			
+		}
+		System.out.println(i);
+	}
+
+}
